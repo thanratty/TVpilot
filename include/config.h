b7b6bd3da053968631f8bab76d8222eaf53eddb6
Tony Hanratty <thanratty@users.noreply.github.com>
2026-03-07 21:23:45 +0000

    Set flags for release build

--- include/config.h
+++ include/config.h
@@ -8,15 +8,15 @@
 #define         CURL_MAX_TRIES                  3       // Max # curl download attempts including retries
 #define         CURL_RETRY_DELAY                2000    // Delay 2 seconds after a Curl download error
 #define         CURL_REPLY_TIMEOUT              8000    // Allow 8 seconds for https transaction to complete
-#define         NAMED_OBJECTS                   1       // Events, Semaphores and Threds are created with names
+#define         NAMED_OBJECTS                   0       // Events, Semaphores and Threds are created with names
 #define         NUMBER_OF_DOWNLOAD_THREADS      15      // Number of worker threads created to do the actual downloading
 
  /**
   * Settings only active for DEBUG configuration
   */
-#define         USE_TEST_DATAFILE               1       // Force use of alternative datafile for testing NB Both DEBUG & RELEASE
+#define         USE_TEST_DATAFILE               0       // Force use of alternative datafile for testing NB Both DEBUG & RELEASE
 #define         ENABLE_CONSOLE_LOGGING          0       // Create a console window for log messages. Finer control in logging.hpp
-#define         ECHO_MSG_WINDOW_TO_CONSOLE      1
+#define         ECHO_MSG_WINDOW_TO_CONSOLE      0
 #define         SAVE_WEBPAGE_ON_ERROR           0       // Creates file webpage.txt if there's an XML parse error [ DEBUG only ]
 #define         PAUSE_BEFORE_EXIT               0       // Wait for <RETURN> key in the console. Allows copying console window contents etc. [DEBUG only]
 #define         TRACE_APP_EVENTS                0       // Print all eAppevents in the message window [ DEBUG only ]
